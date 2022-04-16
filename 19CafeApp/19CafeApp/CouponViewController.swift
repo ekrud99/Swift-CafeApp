1fde8c01ba3670cecbc6dd3ef1a2e5b688fa892d
origin <dagyeong33@gmail.com>
2022-04-16 22:33:50 +0900

    title

---
+++ 19CafeApp/19CafeApp/CouponViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CouponViewController.swift
+//  19CafeApp
+//
+//  Created by 최다경 on 2022/04/16.
+//
+
+import UIKit
+
+class CouponViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.title = "Coupons"
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
